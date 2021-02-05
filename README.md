ce30abef5b55b118dad37797a244894af13ad11f
VIJAY G <gvj116@gmail.com>
2021-02-05 23:50:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Freelance test
+
+1. Clone project
+2. cd myapp
+3. yarn
+4. yarn start
+5. Open browser http://localhost:8000
---
+++ config/routes.ts
@@ -0,0 +1,48 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './User/login',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: '/',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Admin',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Admin',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    component: './404',
+  },
+];
